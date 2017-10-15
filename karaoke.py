eccbb5163b1da53876e68ce48fbeea8b3cf0b336
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-10-15 19:44:18 +0200

    iniciando

--- karaoke.py
+++ karaoke.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from smallsmilhandler import SmallSMILHandler
+
+if __name__ == '__main__':
+
+    parser = make_parser()
+    sHandler = SmallSMILHandler()
+    parser.setContentHandler(sHandler)
+    parser.parse(open('karaoke.smil'))
+    print(sHandler.get_tags()['img']['src'])
