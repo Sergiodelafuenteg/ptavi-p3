413a1d8d5cf355c01ec9d353b8e850ab44291ff0
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-10-15 20:45:28 +0200

    desarrollando

--- karaoke.py
+++ karaoke.py
@@ -4,10 +4,26 @@
 from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
 
+class KaraokeLocal(SmallSMILHandler):
+
+    def __init__(self):
+        parser = make_parser()
+        sHandler = SmallSMILHandler()
+        parser.setContentHandler(sHandler)
+        parser.parse(open('karaoke.smil'))
+        self.tags = sHandler.get_tags()
+
+    def __str__(self):
+
+        stringtag = ""
+        for tag in self.tags:
+            stringtag += tag + '\t'
+            for attr in self.tags[tag]:
+                stringtag += attr + '\t'
+            stringtag += "\n"
+        return stringtag
+
 if __name__ == '__main__':
 
-    parser = make_parser()
-    sHandler = SmallSMILHandler()
-    parser.setContentHandler(sHandler)
-    parser.parse(open('karaoke.smil'))
-    print(sHandler.get_tags()['img']['src'])
+    k = KaraokeLocal()
+    print(k.__str__())
