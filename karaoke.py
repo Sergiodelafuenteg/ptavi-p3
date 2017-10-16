3a7d45da18cdda84d3057490f0de87ed9cd71b2e
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-10-16 21:07:03 +0200

    programado to_json

--- karaoke.py
+++ karaoke.py
@@ -3,6 +3,7 @@
 
 from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
+import json
 
 class KaraokeLocal(SmallSMILHandler):
 
@@ -15,17 +16,27 @@ class KaraokeLocal(SmallSMILHandler):
 
     def __str__(self):
 
-        stringtag = '-'.join(str(self.tags))
-
-
-
+        list_lines = []
+        list_attri = []
+        for tag in self.tags:
+            for attri in tag:
+                for cont in tag[attri]:
+                    linea = "{}='{}'".format(cont, tag[attri][cont])
+                    list_attri.append(linea)
+                linea = "{}\t{}".format(attri, '\t'.join(list_attri))
+                list_lines.append(linea)
+        stringtag = '\n'.join(list_lines)
 
         return stringtag
 
         #tag + '\t' + attr + "=" + cont + \n
         #Elemento1\tAtributo11="Valor11"\tAtributo12="Valor12"\t...\n
         #root-layout\twidth="248"\theight="300"\tbackground-color="blue"\n
+    def to_json(self):
+        with open('karaoke.json', 'w') as outfile:
+            json.dump(self.tags, outfile)
+
 if __name__ == '__main__':
 
-    k = KaraokeLocal()
-    print(k)
+    karaoke = KaraokeLocal()
+    karaoke.to_json()
